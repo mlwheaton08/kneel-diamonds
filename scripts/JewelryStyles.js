7d8d8dbb766401004cd08d45dd56d5b9b4d8a996
mlwheaton08 <mlwheaton06@gmail.com>
2022-09-06 21:34:11 -0500

    first commit. project completed

---
+++ scripts/JewelryStyles.js
@@ -0,0 +1,33 @@
+import { getStyles, setStyle } from "./database.js"
+
+const styles = getStyles()
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.name === "style") {
+            setStyle(parseInt(event.target.value))
+        }
+    }
+)
+
+export const JewelryStyles = () => {
+    let html = "<ul>"
+
+    // Use .map() for converting objects to <li> elements
+    const listItems = styles.map(
+        object => {
+            return `<li>
+            <input type="radio" name="style" value="${object.id}" /> ${object.style}
+        </li>`
+        }
+    )
+
+
+    // Join all of the strings in the array into a single string
+    html += listItems.join("")
+
+    html += "</ul>"
+    return html
+}
+
